cc36299d085dba0eba430e901c14b9df856610e6
Joe Dempsey <jdemps22@students.kennesaw.edu>
2020-04-22 00:03:11 -0400

    Began the process for generating characters

---
+++ TabletopHelper.Characters.CharacterGenerator/Generator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TabletopHelper.Characters.Library;
+
+namespace TabletopHelper.Characters.CharacterGenerator
+{
+    public static class Generator
+    {
+        //allow for values to be assigned to generate characters
+        public static Character GenerateNewCharacter()
+        {
+            //test values to be abstrated later
+            Character newCharacter = new Character
+            {
+                CharacterName = "Test Character",
+                PlayerName = "Test Player",
+                Race = "Human",
+                Background = "Noble",
+                Alignment = "LG",
+                Classes = new List<string> { "Barbarian" },
+                AbilityScores = new List<AbilityScore>
+                {
+                    new AbilityScore { ScoreName = "Strength", ScoreValue = 10, ScoreModifier = 0 },
+                    new AbilityScore { ScoreName = "Dexterity", ScoreValue = 10, ScoreModifier = 0 },
+                    new AbilityScore { ScoreName = "Constitution", ScoreValue = 10, ScoreModifier = 0 },
+                    new AbilityScore { ScoreName = "Wisdom", ScoreValue = 10, ScoreModifier = 0 },
+                    new AbilityScore { ScoreName = "Intelligence", ScoreValue = 10, ScoreModifier = 0 },
+                    new AbilityScore { ScoreName = "Charisma", ScoreValue = 10, ScoreModifier = 0 }
+                },
+                ArmorClass = 10,
+                ExperiencePoints = 0,
+                HitPoints = new HitPoints
+                {
+                    MaximumHealth = 10,
+                    CurrentHealth = 10,
+                    TemporaryHealth = 0
+                },
+                Initiative = 0,
+                
+            };
+
+            return newCharacter;
+        }
+    }
+}
---
+++ TabletopHelper.Characters.CharacterGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TabletopHelper.Characters.CharacterGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test 
+            var character = Generator.GenerateNewCharacter();
+            Console.WriteLine("Name: " + character.CharacterName);
+            Console.WriteLine("Player Name: " + character.PlayerName);
+            Console.WriteLine("HP: " + character.HitPoints.CurrentHealth);
+            Console.WriteLine("Race: " + character.Race);
+            Console.WriteLine("Class: " + character.Classes.First());
+            Console.WriteLine("AC: " + character.ArmorClass);
+            Console.WriteLine("Background: " + character.Background);
+            Console.WriteLine("Alignment: " + character.Alignment);
+        }
+    }
+}
