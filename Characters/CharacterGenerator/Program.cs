0ef585d719bf5285b66eeb74fa6169442f412923
Joe Dempsey <jdemps22@students.kennesaw.edu>
2020-04-24 00:43:47 -0400

    Began implementation of a connection to an api endpoint to pull data from to manipulate

--- Characters/CharacterGenerator/Program.cs
+++ Characters/CharacterGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TabletopHelper.Data.APIConnector;
 
 namespace TabletopHelper.Characters.CharacterGenerator
 {
@@ -8,43 +9,7 @@ namespace TabletopHelper.Characters.CharacterGenerator
         {
             //eventually clear out this area to further abstract
             //add further classes and/or projects to control further aspects, ie playable class management, inventory configuration, attack creation, etc.
-
-            Console.WriteLine("Input Player Name: ");
-            string playerName = Console.ReadLine();
-            Console.WriteLine("Input Character Name: ");
-            string characterName = Console.ReadLine();
-            Console.WriteLine("Input Race: ");
-            string race = Console.ReadLine();
-            Console.WriteLine("Input Class: ");
-            string className = Console.ReadLine();
-            Console.WriteLine("Input Background: ");
-            string background = Console.ReadLine();
-            Console.WriteLine("Input Alignment: ");
-            string alignment = Console.ReadLine();
-            Console.WriteLine("Input Strength: ");
-            int strength = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Input Dexterity: ");
-            int dexterity = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Input Constitution: ");
-            int constitution = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Input Wisdom: ");
-            int wisdom = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Input Intelligence: ");
-            int intelligence = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Input Charisma: ");
-            int charisma = Int32.Parse(Console.ReadLine());
-
-            var character = Generator.GenerateNewCharacter
-                (characterName, playerName, race, 
-                className, background, alignment,
-                strength, dexterity, constitution,
-                wisdom, intelligence, charisma);
-
-            Console.WriteLine("Modifiers:");
-            foreach(var i in character.AbilityScores)
-            {
-                Console.WriteLine("{0}: {1}, {2}", i.ScoreName, i.ScoreValue, i.ScoreModifier);
-            }
+            Console.WriteLine(GetEquipment.GetAllWeapons());
         }
     }
 }
---
+++ Data/APIConnector/GetEquipment.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+
+namespace TabletopHelper.Data.APIConnector
+{
+    public static class GetEquipment
+    {
+        //include newtonsoft to convert from json string to parsable and queriable lists
+        private static string URL = "http://www.dnd5eapi.co/api/equipment-categories/weapon";
+
+        public static string GetAllWeapons()
+        {
+            string content;
+
+            using (HttpClient client = new HttpClient())
+            {
+                content = client.GetStringAsync(URL).Result;
+            }
+            return content;
+        }
+        public static string GetSearchedWeapon(string weaponName)
+        {
+            /* 
+             * use newtonsoft to get the list of all weapons
+             * and parse it for specific weapon type and use available returned url
+             * to get further content 
+             */
+            string content = "";
+            return content;
+        }
+    }
+}
