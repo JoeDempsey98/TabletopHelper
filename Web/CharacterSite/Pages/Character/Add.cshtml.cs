00e6c32b6a68e0d72c8e00256d1e455a62e15dfd
Joe Dempsey <jdemps22@students.kennesaw.edu>
2020-04-27 00:52:06 -0400

    Began implementation of razor pages for siteside character management

---
+++ Web/CharacterSite/Pages/Character/Add.cshtml.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CharacterSite.Pages.Character
+{
+    public class AddModel : PageModel
+    {
+        //inject class for creating characters, and uploading them to database/cloud
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- Web/CharacterSite/Startup.cs
+++ Web/CharacterSite/Startup.cs
@@ -24,6 +24,7 @@ namespace CharacterSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            //add in a singleton for crud character service
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
